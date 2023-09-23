fe1800e0b6d6af03660fc76707388ed129056aaf
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2023-09-23 09:52:26 -0300

    Codigo
    Codigo 22/09/2023

--- Proyect_Versus/src/Interfaz/Proyect_versus.java
+++ Proyect_Versus/src/Interfaz/Proyect_versus.java
@@ -9,10 +9,6 @@ public class Proyect_versus {
 	public static void main(String[] args) {
 	
 	int opcion = 0;	
-    Habilidad habilidad1 = new Habilidad (null, null, 0);
-	Partida partida1 = new Partida (null, null);	
-	Personaje personaje1 = new Personaje (null, null);
-	SistemaJuego sistema1 = new SistemaJuego ();
 	Usuario usuario1 = new Usuario (null,null);
 	
 	do {
