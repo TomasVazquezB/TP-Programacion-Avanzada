65d0760b71a3a2996ef7d2c0ed2af82e441232ec
TomasVazquezB <vbtomas@hotmail.com>
2023-09-26 10:45:13 -0300

    Codigo
    Actualizacion dia 26/09/2023

--- Proyect_Versus/src/Interfaz/Proyect_versus.java
+++ Proyect_Versus/src/Interfaz/Proyect_versus.java
@@ -8,77 +8,87 @@ public class Proyect_versus {
 
 	public static void main(String[] args) {
 	
-	int opcion = 0;	
 	Usuario usuario1 = new Usuario (null,null);
+	SistemaJuego sistema = new SistemaJuego ();
 	
-	do {
+	while (true) {
+	
+		String[] opciones =  {"Registrar Usuario","Iniciar Sesion","Salir"};
+		String [] opciondeingreso = {"Si","No"};
 		
-		String [] opcionesdeinicio = {"Usuario"};
-	    int selecciondeinicio;
-	    selecciondeinicio = JOptionPane.showOptionDialog(null,"Bienvenido al menu del juego" ,"Menu de inicio",JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE ,null, opcionesdeinicio,opcionesdeinicio[1]);
+		int seleccion =JOptionPane.showOptionDialog(null,"Bienvenido al menu del juego" ,"Menu de inicio",JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE ,null, opciones,opciones[0]);
 		
-		switch (selecciondeinicio) {
+		
+		switch (seleccion) {
 		
 		case 0: 
+			
 			
 			JOptionPane.showMessageDialog(null, "Registrese con sus datos","Registro",JOptionPane.QUESTION_MESSAGE);
 			
 			String nombredeusuario,contrasenadeusuario;
-	
+			
 			nombredeusuario = JOptionPane.showInputDialog(null,"Ingrese el nombre de usuario que desea","Registro nombre",JOptionPane.QUESTION_MESSAGE);
 			usuario1.setNombre(nombredeusuario);
-		    contrasenadeusuario = JOptionPane.showInputDialog(null,"Ingrese la contraseña que va a utilizar","Registro contraseña",JOptionPane.QUESTION_MESSAGE);
-		    usuario1.setContrasena(contrasenadeusuario);
+			contrasenadeusuario = JOptionPane.showInputDialog(null,"Ingrese la contraseña que va a utilizar","Registro contraseña",JOptionPane.QUESTION_MESSAGE);
+			usuario1.setContrasena(contrasenadeusuario);
 		
-		    if (usuario1.iniciarSesion("","")!=null) {
-				
-		    	JOptionPane.showMessageDialog(null, "Regristro exitoso", "Registro finalizado",JOptionPane.QUESTION_MESSAGE);
-		    	
-			}else {
-				
-				JOptionPane.showMessageDialog(null, "Regristro fallido", "Error",JOptionPane.QUESTION_MESSAGE);
-				
-			}
-		
-		break;
-		
-	case 1:
-		
-		JOptionPane.showMessageDialog(null, "Ingrese sus datos para iniciar sesion", "Ingreso de datos",JOptionPane.QUESTION_MESSAGE);
-		usuario1.iniciarSesion(null, null);
-		
-		 if (usuario1.iniciarSesion("","")!=null) {
-				
-			 JOptionPane.showMessageDialog(null, "Ingreso exitoso","Ingreso",JOptionPane.QUESTION_MESSAGE);
-			 Usuario registrado = usuario1.iniciarSesion("","");
-			 int opcion2 = JOptionPane.showOptionDialog(null,"Que desea realizar?\n" + "1 Armar equipo\n" +  "2 Jugar una partida\n" ,"Opciones",JOptionPane.DEFAULT_OPTION, 0, null,opcionesdeinicio,opcionesdeinicio[0]);
-		
+			if (usuario1.iniciarSesion("","")==null) {
+				JOptionPane.showMessageDialog(null, "Registro fallido", "Error", JOptionPane.ERROR_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Registro exitoso", "Registro finalizado", JOptionPane.INFORMATION_MESSAGE);
 			}
 			
 			break;
 			
-	case 2: 
-		
-		JOptionPane.showMessageDialog(null, "Lista de equipos que tiene:", "Equipos",JOptionPane.QUESTION_MESSAGE);
-		
-		JOptionPane.showMessageDialog(null, "Los personajes que tiene para elegir son:", "Personajes", JOptionPane.QUESTION_MESSAGE);
-		
-		JOptionPane.showMessageDialog(null, "Esta seguro que quiere guardar ese equipo? Si o No", "Guardar equipo", JOptionPane.QUESTION_MESSAGE);
-		
-		
-		break;
-	
-	case 3: 
-		
-		JOptionPane.showMessageDialog(null, "Con que equipo desea jugar?", "Listado de equipos", JOptionPane.QUESTION_MESSAGE);
-		
-	    JOptionPane.showMessageDialog(null, "La partida va a empezar, esta listo", "Partida iniciando", JOptionPane.QUESTION_MESSAGE);
-		
-	    break; 
-		
+		case 1:
+			
+			//Si no ingreso que no pueda ver las opciones, y si ingreso que pueda ver las opciones
+			
+			JOptionPane.showMessageDialog(null, "Ingrese sus datos para iniciar sesion", "Ingreso de datos",JOptionPane.QUESTION_MESSAGE);
+			usuario1.iniciarSesion("","");
+			
+			 if (usuario1.iniciarSesion("","")!=null) {
+					
+				 JOptionPane.showMessageDialog(null, "Ingreso exitoso","Ingreso",JOptionPane.QUESTION_MESSAGE);
+				 Usuario registrado = usuario1.iniciarSesion("","");
+				 int opcion2 = JOptionPane.showOptionDialog(null,"Que desea realizar?\n" + "1 Armar equipo\n" +  "2 Jugar una partida\n" ,"Opciones",JOptionPane.DEFAULT_OPTION, 0, null,opciones,opciones[0]);
+			
+				}
+				
+				break;
+						
+		case 2:
+			
+			JOptionPane.showMessageDialog(null, "Lista de equipos que tiene:", "Equipos",JOptionPane.QUESTION_MESSAGE);
+			
+			JOptionPane.showMessageDialog(null, "Los personajes que tiene para elegir son:", "Personajes", JOptionPane.QUESTION_MESSAGE);
+			
+		 
+			
+		   JOptionPane.showConfirmDialog(null, "Esta seguro que quiere guardar ese equipo? Si o No", "Guardar equipo", JOptionPane.YES_NO_OPTION);
+			
+			break;
+			
+			
+		case 3:
+			
+			JOptionPane.showMessageDialog(null, "Con que equipo desea jugar?", "Listado de equipos", JOptionPane.QUESTION_MESSAGE);
+			
+		    JOptionPane.showMessageDialog(null, "La partida va a empezar, esta listo", "Partida iniciando", JOptionPane.QUESTION_MESSAGE);
+			
+			break;
+				
+		case 4: 
+			
+	    JOptionPane.showMessageDialog(null, "Gracias por jugar. ¡Hasta Luego!");
+			
+	    System.exit(0);
+			
+		default:
+			break;
 		}
-
-	} while (opcion == 6);
-		
+	
 	}
+}
 }
