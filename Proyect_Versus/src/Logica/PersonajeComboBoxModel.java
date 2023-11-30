e8c5045767979f586ae777f8dc93cb1f11f8a3ef
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2023-11-30 12:34:14 -0300

    Actualizacion Codigo 30/11/2023
    Actualizacion Codigo 30/11/2023

--- Proyect_Versus/src/Logica/PersonajeComboBoxModel.java
+++ Proyect_Versus/src/Logica/PersonajeComboBoxModel.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class PersonajeComboBoxModel extends DefaultComboBoxModel<Object> {
 
-    public PersonajeComboBoxModel(List<Personaje> personajes) {
+    private static final long serialVersionUID = -3986223225467606847L;
+
+	public PersonajeComboBoxModel(List<Personaje> personajes) {
         super(personajes.toArray());
     }
 
